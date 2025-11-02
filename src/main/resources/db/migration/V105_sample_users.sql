597281df68f8a8f8a8a92832a0e8c46d57a0fad6
seongho-yun <tgv5612@gmail.com>
2025-11-02 13:38:31 +0900

    feat: 샘플 User 데이터추가

---
+++ src/main/resources/db/migration/V105_sample_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, login_id, password) VALUES
+    ('김준성', 'kimjs', 'test123'),
+    ('임상진', 'imsj', 'test123'),
+    ('윤성호', 'yunsh', 'test123'),
+    ('홍길동', 'honggd', 'test123'),
+    ('조하늘', 'johnl', 'test123'),
+    ('이태웅', 'leetw', 'test123'),
+    ('김민준', 'kimmj', 'test123'),
